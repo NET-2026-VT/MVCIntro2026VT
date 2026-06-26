418b30bb29b0316b0c873428ea99be780671529c
Jonathan Krall <jonathan.krall@lexicon.se>
2026-06-26 11:15:13 +0200

    Added PeopleController and updated Default Route

---
+++ MVCIntro2026VT/Controllers/PeopleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCIntro2026VT.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MVCIntro2026VT/Program.cs
+++ MVCIntro2026VT/Program.cs
@@ -22,7 +22,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=People}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
